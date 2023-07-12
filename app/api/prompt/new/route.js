8c74998d97b7bf31d87918161212252bd9f2af39
Jagadeesh <84759769+Jagadeesh-tamizh@users.noreply.github.com>
2023-07-12 11:26:17 +0530

    Update route.js
    Testing routes

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -2,7 +2,9 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const POST = async (req) => {
+  console.log("Post route called succesfully");
   const { userId, prompt, tag } = await req.json();
+  
 
   try {
     await connectToDB(); //Fixed Typo
